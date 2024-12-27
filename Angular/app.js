68ecdc4b3f84d79e659ca8ded5d97aae414e5336
Alan Angelico Malolos <xandrexei@gmail.com>
2024-12-27 13:37:20 +0800

    Update
    Angular

---
+++ Angular/app.js
@@ -0,0 +1,26 @@
+const list = angular.module("propertyList", []);
+list.controller("propertySearch", function ($scope, $http) {
+	$http.get("app.php").then(function (response) {
+		$scope.properties = response.data.slice(1);
+		$scope.propertyTypes = [...new Set($scope.properties.map((p) => p.property_type))];
+
+		// Initialize filters
+		$scope.searchQuery = "";
+
+		// Change the selected property type
+		$scope.changeSelected = function (property) {
+			$scope.selectedProperty = property;
+		};
+
+		// Filter function to match the selected property type
+		$scope.propertyFilter = function (property) {
+			return !$scope.selectedProperty || property.property_type === $scope.selectedProperty;
+		};
+	});
+});
+
+function CustomFilter(obj) {
+	this.removeDuplicate = function () {
+		return obj.filter((value, index, self) => index === self.findIndex((t) => t.property_type === value.property_type));
+	};
+}
---
+++ Angular/app.php
@@ -0,0 +1,34 @@
+<?php
+// Database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "properties";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to fetch data
+$sql = "SELECT * FROM property";
+$result = $conn->query($sql);
+
+// Check if there are results
+if ($result->num_rows > 0) {
+    header('Content-Type: application/json');
+    // Output data of each row
+    $data = [];
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo json_encode([]);
+}
+
+// Close connection
+$conn->close();
