d288a18d95ce9546e19b82fdc08b89cc9e8bbed6
Alan Angelico Malolos <xandrexei@gmail.com>
2026-09-12 20:40:43 +0800

    update
    test

--- Payload/php/payload.php
+++ Payload/php/payload.php
@@ -1,3 +1,13 @@
 <?php
+
 $payload = file_get_contents('php://input');
-echo $payload;
+
+echo '<h2>Payload</h2>';
+echo '<pre>';
+echo htmlspecialchars($payload);
+echo '</pre>';
+
+echo '<h2>Content Type</h2>';
+echo '<pre>';
+echo htmlspecialchars($_SERVER['CONTENT_TYPE'] ?? 'Unknown');
+echo '</pre>';
